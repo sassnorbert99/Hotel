4b163576cb610fe2f5a562712e702dfcc69ea598
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 22:21:49 +0100

    KiadottSzoba.cs nevű osztály létrehozva, kiegészítve

---
+++ prototype/Hotel_prototype/Hotel_prototype/KiadottSzoba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_prototype
+{
+    class KiadottSzoba
+    {
+        string nev;
+
+        public string Nev
+        {
+            get
+            {
+                return nev;
+            }
+            set
+            {
+                nev = value;
+            }
+        }
+
+        int kivettSzoba;
+
+        public int KivettSzoba
+        {
+            get
+            {
+                return kivettSzoba;
+            }
+            set
+            {
+                kivettSzoba = value;
+            }
+        }
+
+        public KiadottSzoba(string _nev, int _kivettSzoba)
+        {
+            Nev = _nev;
+            KivettSzoba = _kivettSzoba;
+        }
+    }
+}
