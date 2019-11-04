28aa0c7f2cfcae3e13ce7578b6cd7308c61a016d
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 20:16:40 +0100

    prototípus: szobák nevű osztály létrehozva, kibővítve

---
+++ prototype/Hotel_prototype/Hotel_prototype/Szobák.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_prototype
+{
+    class Szobák
+    {
+        int szobaszam;
+
+        public int Szoba
+        {
+            get
+            {
+                return szobaszam;
+            }
+            set
+            {
+                szobaszam = value;
+            }
+        }
+
+        int agyszam;
+
+        public int Agyszam
+        {
+            get
+            {
+                return agyszam;
+            }
+            set
+            {
+                agyszam = value;
+            }
+        }
+
+        felszereltseg felszereles;
+
+        public felszereltseg Felszereles
+        {
+            get
+            {
+                return felszereles;
+            }
+            set
+            {
+                felszereles = value;
+            }
+        }
+    }
+}
