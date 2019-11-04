b845508fc6db0c7e9204c62ee5e15fcfd80fca62
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 20:17:33 +0100

    prototype: felszereltseg nevű enum létrehozva, elemekkel feltöltve

---
+++ prototype/Hotel_prototype/Hotel_prototype/felszereltseg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_prototype
+{
+    enum felszereltseg
+    {
+        LUXUS,
+        KOMFORTOS,
+        ÁTLAGOS,
+        FAPADOS
+    }
+}
