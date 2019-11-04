c3d733f45d33209ba5c6872837eb696351559672
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 20:56:45 +0100

    Form1.cs kidolgozva, hibák javítva

--- prototype/Hotel_prototype/Hotel_prototype/Form1.cs
+++ prototype/Hotel_prototype/Hotel_prototype/Form1.cs
@@ -15,11 +15,19 @@ namespace Hotel_prototype
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Random rnd = new Random();
+            Vendég a = new Vendég("Sass-Gyarmati Norbert", "123456789", "3413. Cserépfalu xy út 00", 1);
+            List<Szobák> szoba = new List<Szobák>();
+            for (int i = 0; i < 10; i++)
+            {
+                Szobák c = new Szobák(i, rnd.Next(1, 5), felszereltseg.KOMFORTOS);
+                szoba.Add(c);
+            }
         }
     }
 }
