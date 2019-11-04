90c54890fb19fabd23a2228c88d3b8a00dfbac51
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 20:36:58 +0100

    prototípus Vendég osztály létrehozva, kiegészítve

---
+++ prototype/Hotel_prototype/Hotel_prototype/Vendég.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_prototype
+{
+    class Vendég
+    {
+        string nev;
+
+        public string Nev
+        {
+            get
+            {
+                return nev;
+            }
+            set
+            {
+                nev = value;
+            }
+        }
+
+        string taj;
+
+        public string Taj
+        {
+            get
+            {
+                return taj;
+            }
+            set
+            {
+                taj = value;
+            }
+        }
+
+        string lakcim;
+
+        public string Lakcim
+        {
+            get
+            {
+                return lakcim;
+            }
+            set
+            {
+                lakcim = value;
+            }
+        }
+
+        int kivettSzoba;
+
+        public int KivettSzoba
+        {
+            get
+            {
+                return kivettSzoba;
+            }
+            set
+            {
+                kivettSzoba = value;
+            }
+        }
+
+        public Vendég(string _nev, string _taj, string _lakcim, int _kivettSzoba)
+        {
+            Nev = _nev;
+            Taj = _taj;
+            Lakcim = _lakcim;
+            KivettSzoba = _kivettSzoba;
+        }
+
+        public override string ToString()
+        {
+            return "név: "+Nev+" taj száma: "+Taj+" lakcíme: "+Lakcim+" kivett szoba: "+KivettSzoba;
+        }
+    }
+}
