7746faf27037bf568b81b034ff2b5bb3a5087263
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 21:23:59 +0100

    design rész elkezdve

--- prototype/Hotel_prototype/Hotel_prototype/Form1.Designer.cs
+++ prototype/Hotel_prototype/Hotel_prototype/Form1.Designer.cs
@@ -28,21 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.vendégek = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(118, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(681, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // vendégek
+            // 
+            this.vendégek.Enabled = false;
+            this.vendégek.Location = new System.Drawing.Point(12, 12);
+            this.vendégek.Name = "vendégek";
+            this.vendégek.Size = new System.Drawing.Size(100, 20);
+            this.vendégek.TabIndex = 1;
+            this.vendégek.Text = "vendégek";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vendégek);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox vendégek;
     }
 }
 
