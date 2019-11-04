c10e410763bb945bbb728d30b99c73e151a3cf00
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 14:06:38 +0100

    megegyzések, kiegészítések írása a Database.java-hoz

--- Project/HotelSouceCode/src/DataSource/Database.java
+++ Project/HotelSouceCode/src/DataSource/Database.java
@@ -3,6 +3,8 @@ package DataSource;
 public class Database {
 
     public static final String DB_NAME = "Hotel.db";
+    /*connect to windows*/
     public static final String CONNECTION_STRING_WIN = "jdbc:sqlite:..\\Database\\" + DB_NAME;
+    /*connect to MacOS*/
     public static final String CONNECTION_STRING_MAC = "jdbc:sqlite:../Database/" + DB_NAME;
 }
