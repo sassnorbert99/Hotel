397ba85f83a4ce82624216cdf8bec549f0026ef4
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 22:19:25 +0100

    design része feldolgozás alatt

--- prototype/Hotel_prototype/Hotel_prototype/Form1.Designer.cs
+++ prototype/Hotel_prototype/Hotel_prototype/Form1.Designer.cs
@@ -30,6 +30,20 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.vendégek = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.nevbox = new System.Windows.Forms.TextBox();
+            this.tajbox = new System.Windows.Forms.TextBox();
+            this.lakbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.szobabox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -50,11 +64,145 @@
             this.vendégek.TabIndex = 1;
             this.vendégek.Text = "vendégek";
             // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "szobák";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(118, 39);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(681, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "kivett szobák";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(118, 66);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(681, 21);
+            this.comboBox3.TabIndex = 5;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(12, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "regisztráció";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(12, 180);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "név";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(12, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "taj száma";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(12, 232);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "lakcíme";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(12, 258);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 10;
+            this.textBox7.Text = "szabad szoba";
+            // 
+            // nevbox
+            // 
+            this.nevbox.Location = new System.Drawing.Point(118, 180);
+            this.nevbox.Name = "nevbox";
+            this.nevbox.Size = new System.Drawing.Size(237, 20);
+            this.nevbox.TabIndex = 13;
+            this.nevbox.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // tajbox
+            // 
+            this.tajbox.Location = new System.Drawing.Point(118, 206);
+            this.tajbox.Name = "tajbox";
+            this.tajbox.Size = new System.Drawing.Size(237, 20);
+            this.tajbox.TabIndex = 14;
+            // 
+            // lakbox
+            // 
+            this.lakbox.Location = new System.Drawing.Point(118, 232);
+            this.lakbox.Name = "lakbox";
+            this.lakbox.Size = new System.Drawing.Size(237, 20);
+            this.lakbox.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 72);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "beküld";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // szobabox
+            // 
+            this.szobabox.Location = new System.Drawing.Point(118, 258);
+            this.szobabox.Name = "szobabox";
+            this.szobabox.Size = new System.Drawing.Size(237, 20);
+            this.szobabox.TabIndex = 17;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.szobabox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lakbox);
+            this.Controls.Add(this.tajbox);
+            this.Controls.Add(this.nevbox);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.vendégek);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
@@ -69,6 +217,20 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox vendégek;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox nevbox;
+        private System.Windows.Forms.TextBox tajbox;
+        private System.Windows.Forms.TextBox lakbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox szobabox;
     }
 }
 
