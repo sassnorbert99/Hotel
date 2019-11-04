8ec95fb28d031b589e3765ce2049499b31be9a64
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-04 14:17:50 +0100

    további hibák javítva

--- Project/HotelSouceCode/src/DataSource/DBConnection.java
+++ Project/HotelSouceCode/src/DataSource/DBConnection.java
@@ -1,3 +1,5 @@
+/*SQL database*/
+
 package DataSource;
 
 import java.sql.*;
@@ -10,10 +12,11 @@ public class DBConnection {
         String OSName = System.getProperty("os.name").toLowerCase();
 
         try {
-
+            /*connection_string_win method*/
             if (OSName.contains("win")){
                 conn = DriverManager.getConnection(Database.CONNECTION_STRING_WIN);
             }
+            /*connection_string_mac method*/
             else if(OSName.contains("mac")){
                 conn = DriverManager.getConnection(Database.CONNECTION_STRING_MAC);
             }
