d348c75dca08e7c530c320b4c57e6dbf8177d129
Oravecz Zsolt <zsolti1012@gmail.com>
2019-11-05 09:28:40 +0100

    Program.cs

--- prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Form1.cs
+++ prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Form1.cs
@@ -13,9 +13,12 @@ namespace Beléptető_prototype
     public partial class Form1 : Form
     {
         public List<Users> Users_list = new List<Users>();
+        
         public Form1()
         {
             InitializeComponent();
+            Users a = new Users("a", "1");
+            Users_list.Add(a);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -35,11 +38,40 @@ namespace Beléptető_prototype
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            if textBo
-
-            foreach (Users item in Users_list)
+            String Username;
+            string Password;
+            bool success = false;
+            Username = textBox1.Text;
+            Password = textBox2.Text;
+            MessageBox mb;
+            if (Username == "" || Password == "")
             {
-                
+                MessageBox.Show("Felhasználónév és jelszó megadása kötelező!");
+            }
+            else
+            {
+
+
+                foreach (Users item in Users_list)
+                {
+                    if (item.Username == Username && item.Password == Password)
+                    {
+                        success = true;
+                    }
+                }
+
+                if (success)
+                {
+                    MessageBox.Show("Sikeres bejelentkezés!");
+                    Process
+                }
+
+                else
+                {
+                    MessageBox.Show("Sikertelen bejelentkezés!");
+                    textBox1.Clear();
+                    textBox2.Clear();
+                }
             }
         }
     }
--- prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Program.cs
+++ prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Program.cs
@@ -17,6 +17,7 @@ namespace Beléptető_prototype
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
