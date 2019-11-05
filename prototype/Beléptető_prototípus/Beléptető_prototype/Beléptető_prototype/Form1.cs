8967a63fa85e3fcb7caf856bfe94265a0326db45
Oravecz Zsolt <zsolti1012@gmail.com>
2019-11-05 10:08:16 +0100

    Prototype

--- prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Form1.cs
+++ prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -63,7 +64,7 @@ namespace Beléptető_prototype
                 if (success)
                 {
                     MessageBox.Show("Sikeres bejelentkezés!");
-                    Process
+                    Process.Start("");
                 }
 
                 else
---
+++ prototype/Beléptető_prototípus/Beléptető_prototype/Beléptető_prototype/Users.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beléptető_prototype
+{
+    public class Users
+    {
+        private int id = 1;
+        public int Id
+        {
+            get { return id; }
+            
+            
+        }
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = Username;
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = Password; }
+        }
+
+        public Users(string username,string password)
+        {
+            id = id + 1;
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
