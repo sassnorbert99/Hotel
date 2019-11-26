b397f1055c9b243bb4b090adaf55b4de35c636ca
Oravecz Zsolt <zsolti1012@gmail.com>
2019-11-26 11:00:28 +0100

    fgfhgbf

---
+++ Project/HotelLite/HotelLite/Login.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotelLite
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
