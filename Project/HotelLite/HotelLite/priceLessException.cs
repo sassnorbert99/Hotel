02d003fe976d71e35769fc975df04e5540a5702c
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-26 10:19:34 +0100

    room.cs osztály létrehozva

---
+++ Project/HotelLite/HotelLite/priceLessException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HotelLite
+{
+    [Serializable]
+    internal class priceLessException : Exception
+    {
+        public priceLessException()
+        {
+        }
+
+        public priceLessException(string message) : base(message)
+        {
+        }
+
+        public priceLessException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected priceLessException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
