3142897d38c390f9d93b662ab4f12f6fa9b79c71
sassnorbert99 <sassnorbert99@gmail.com>
2019-11-26 11:37:40 +0100

    osztályok, hibák hozzáadva exceptions.txt hozzáadva

---
+++ Project/HotelLiteOriginal/HotelLiteOriginal/Entry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelLite
+{
+     class Entry
+    {
+        private string loginName="Hotel";
+        public string LoginName
+        {
+            get
+            {
+                return loginName;
+            }
+
+           
+        }
+
+        private string passwd= "-231202094";
+        public string Passwd
+        {
+            get
+            {
+                return passwd;
+            }
+        }
+    }
+}
---
+++ Project/HotelLiteOriginal/HotelLiteOriginal/tinException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace HotelLite
+{
+    [Serializable]
+    internal class tinException : Exception
+    {
+        public tinException()
+        {
+        }
+
+        public tinException(string message) : base(message)
+        {
+        }
+
+        public tinException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected tinException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
